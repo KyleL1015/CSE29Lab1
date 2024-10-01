4ac5fddc3e43c4f074b20e8ba7ee7c01c1e2a298
KyleL1015 <117131834+KyleL1015@users.noreply.github.com>
2024-10-01 20:38:25 +0000

    third hello

--- hello.c
+++ hello.c
@@ -3,9 +3,11 @@
 int main() {
   char hello[] = "Hello!"; 
   char hello2[] = "Different String";
+  char hello3[] = "I made a third string!";
 
   puts(hello);
   puts(hello2);
+  puts(hello3);
 
   return 0;
 }
